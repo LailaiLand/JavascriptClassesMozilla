1b672b1a6917ec3364b1c8f43dfa4bdba027240f
Nikolaipe <nikolaipe@getacademy.no>
2024-01-04 15:19:52 +0100

    Failed some more
    Tried looking at code from a lecture and copying it, but I think the lecture went on a bit too long, and added complexity. Going to try watching the lecture again and grab what I need.

--- Script/Classes/Flag.js
+++ Script/Classes/Flag.js
@@ -1,16 +1,18 @@
-class Flag extends HTMLElement{
+class Flag extends HTMLElement {
   constructor(color1, color2, color3) {
     super();
-    this.color1 = color1;
-    this.color2 = color2;
-    this.color3 = color3;
+    this.attachShadow({ mode: "open" });
+    this.model.color1 = color1;
+    this.model.color2 = color2;
+    this.model.color3 = color3;
+    this.root = document.createElement("div");
+    this.shadowRoot.appendChild(this.root);
+    this.root.innerHTML = /*HTML*/ `
+    <h1 class = "flagBox" style = "background-color: ${this.model.color3}"> P </h1>
+    <h1 class = "flagBox" style = "background-color: ${this.model.color1}"> R </h1>
+    <h1 class = "flagBox" style = "background-color: ${this.model.color2}"> I </h1>
+    <h1 class = "flagBox" style = "background-color: ${this.model.color1}"> D </h1>
+    <h1 class = "flagBox" style = "background-color: ${this.model.color3}"> E </h1>
+    `;
   }
-  
-  innerHTML = /*HTML*/`
-  <h1 class = "flagBox" style = "background-color: ${color3}"> P </h1>
-  <h1 class = "flagBox" style = "background-color: ${color1}"> R </h1>
-  <h1 class = "flagBox" style = "background-color: ${color2}"> I </h1>
-  <h1 class = "flagBox" style = "background-color: ${color1}"> D </h1>
-  <h1 class = "flagBox" style = "background-color: ${color3}"> E </h1>
-  `
 }
