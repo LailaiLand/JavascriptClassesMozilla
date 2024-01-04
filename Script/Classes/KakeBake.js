2d38a60982f255139c8c39bc66c40e0d24545b29
Nikolaipe <nikolaipe@getacademy.no>
2024-01-04 11:25:29 +0100

    Basics
    A HTML-, CSS-, and JS-file.

---
+++ Script/Classes/KakeBake.js
@@ -0,0 +1,22 @@
+class KakeBake {
+  constructor() {
+    // TBD
+  }
+
+  // Instansefelt
+  kakeSmak = "sjokolade";
+  //Instansemetode
+  bakKake() {
+    //følg punktene på oppskriften
+  }
+
+  //Statisk felt
+  static kakeTopping = "glasur";
+  //statisk metode
+  static oppskrift() {
+    //indgredienser og fremgang
+  }
+
+  //privat felt
+  #bekledning = "forkle. BARE forkle";
+}
