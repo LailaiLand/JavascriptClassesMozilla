c3e6b13700a97d9ac878d427a63bbb8ad2f7c12f
Nikolaipe <nikolaipe@getacademy.no>
2024-01-17 09:42:22 +0100

    Created test custom element
    works better when doing what is proper, rather than blindly following Terje

---
+++ Script/Classes/MyCustomElement.js
@@ -0,0 +1,35 @@
+/*testing directly copying from mdn web docs.
+had some trouble defining custom element before, so checking if I did it wrong*/
+
+class MyCustomElement extends HTMLElement {
+    static observedAttributes = ["color", "size"];
+
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        console.log("Skal printe når elementet legges til");
+    }
+
+    disconnectedCallback() {
+        console.log("Skal printe når elementet blir fjernet");
+    }
+
+    adoptedCallback() {
+        console.log("Skal printe når elementet blir flyttet til ny side");
+    }
+
+    attributeCangedCallback(name, oldValue, newValue) {
+        console.log(`Skal printe når ${name} har blitt endret`);
+    }
+
+    updateView(){
+
+    }
+}
+
+/*I see now that what I did wrong, was trying to define  the element within the class.
+I blame this on Terje for showing it that way.
+It does work, but only if you run the class as an expression within an anonymous function*/
+customElements.define("my-custom-element", MyCustomElement);
